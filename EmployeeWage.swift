7ae3670d9fe2315592ac3bd9a25528757d7001be
naveennvm4 <naveennvm4@gmail.com>
2021-04-07 08:23:48 +0530

    [Naveen]Added: calculate monthly wage

--- EmployeeWage.swift
+++ EmployeeWage.swift
@@ -3,16 +3,19 @@ import UIKit
 let fullTime = 1
 let partTime = 2
 let ratePerHour = 20
+let daysInMonth = 20
 let empCheck = Int.random(in: 1..<3)
 swith empCheck{
 case fullTime:
 	let Hrs = 16
-	let salary = ratePerHour*Hrs
+	let dailyWage = ratePerHour*Hrs
+	let salary = dailyWage*daysInMonth
 	print(salary)
-	print("FullTime Employee")\
+	print("FullTime Employee")
 case partTime:
 	let Hrs = 8
-	let salary = ratePerHour*Hrs
+	let dailyWage = ratePerHour*Hrs
+        let salary = dailyWage*daysInMonth
 	print(salary)
 	print("PartTime Employee")
 default:
