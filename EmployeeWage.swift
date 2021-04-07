6a4903fbea9a0c63211f96ec5616610f350777bc
naveennvm4 <naveennvm4@gmail.com>
2021-04-07 07:47:13 +0530

    [Naveen]Added:Part time employee

--- EmployeeWage.swift
+++ EmployeeWage.swift
@@ -1,14 +1,21 @@
 import UIKit
 
-let isPresent = 1
+let fullTime = 1
+let partTime = 2
 let ratePerHour = 20
-let Hrs = 8
 let empCheck = Int.random(in: 1..<3)
-if(empCheck == isPresent){
+if(empCheck == fullTime){
+	let Hrs = 16
 	let salary = ratePerHour*Hrs
 	print(salary)
-	print("Employee is Present")
+	print("FullTime Employee")
 {
+else if(empCheck == partTime){
+	let Hrs = 8
+	let salary = ratePerHour*Hrs
+	print(salary)
+	print("PartTime Employee")
+}
 else{
-	print("Employee is Absent")
+	print("salary=0")
 }
