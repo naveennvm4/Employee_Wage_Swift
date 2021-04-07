02c7eaf649e1168046431e8335e6e2fa11ec5f93
naveennvm4 <naveennvm4@gmail.com>
2021-04-07 08:13:40 +0530

    [Naveen]Added: Solved using switch statement

--- EmployeeWage.swift
+++ EmployeeWage.swift
@@ -4,18 +4,17 @@ let fullTime = 1
 let partTime = 2
 let ratePerHour = 20
 let empCheck = Int.random(in: 1..<3)
-if(empCheck == fullTime){
+swith empCheck{
+case fullTime:
 	let Hrs = 16
 	let salary = ratePerHour*Hrs
 	print(salary)
-	print("FullTime Employee")
-{
-else if(empCheck == partTime){
+	print("FullTime Employee")\
+case partTime:
 	let Hrs = 8
 	let salary = ratePerHour*Hrs
 	print(salary)
 	print("PartTime Employee")
-}
-else{
+default:
 	print("salary=0")
 }
