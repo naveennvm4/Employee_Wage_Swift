82471f23474d389531530770c702ec8299fba7f3
naveennvm4 <naveennvm4@gmail.com>
2021-04-07 07:26:50 +0530

    [Naveen]Added:Calculating Daily Employee Wage

--- EmployeeWage.swift
+++ EmployeeWage.swift
@@ -1,8 +1,12 @@
 import UIKit
 
 let isPresent = 1
-var empCheck = Int.random(in: 1..<3)
+let ratePerHour = 20
+let Hrs = 8
+let empCheck = Int.random(in: 1..<3)
 if(empCheck == isPresent){
+	let salary = ratePerHour*Hrs
+	print(salary)
 	print("Employee is Present")
 {
 else{
